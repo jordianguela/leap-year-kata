af9b5111aa9df6c1c69c123fb96d927cf41b789e
Jordi Anguela <jordi.anguela@gmail.com>
2016-04-17 20:21:23 +0200

    refactor tests

--- test/leap-year.spec.js
+++ test/leap-year.spec.js
@@ -2,13 +2,17 @@ var LeapYear = require('../src/LeapYear');
 
 describe("Leap Year calculator", function() {
 
+  var leapYearCalc;
+
+  beforeEach(function() {
+    leapYearCalc = new LeapYear();
+  });
+
   it("returns true when the year is divisible by 4 (i.e: 1996)", function() {
-    var leapYearCalc = new LeapYear();
     expect(leapYearCalc.calculate(1996)).toBe(true);
   });
 
   it("returns false when it is not a leap year (i.e: 2001)", function() {
-    var leapYearCalc = new LeapYear();
     expect(leapYearCalc.calculate(2001)).toBe(false);
   });
 
